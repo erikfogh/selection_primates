5e18405efb79f687aeb0cafaf64c330f52d205b2
erik <qulli.erik@gmail.com>
2025-05-02 09:34:15 +0200

    pilot check

---
+++ scripts/notebook_settings.py
@@ -0,0 +1,15 @@
+#Initial configuration 
+import sys, os, re
+import numpy as np
+import pandas as pd
+import gc
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+#from horizonplot import horizonplot
+import seaborn as sns
+sns.set()
+sns.set_theme()
+sns.set_style("white")
+sns.set_context("notebook")
