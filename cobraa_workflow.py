5ce587b07f4b5c3370c72d312c46b5d5c2d22a4b
erik <qulli.erik@gmail.com>
2025-09-09 15:27:27 +0200

    Updated workflows

---
+++ cobraa_workflow.py
@@ -0,0 +1,268 @@
+from gwf import Workflow, AnonymousTarget
+import os
+import pandas as pd
+import numpy as np
+import glob
+
+gwf = Workflow()
+
+# Paths. Autosome implementation. This setup is optimized for the primate_diversity directory setup.
+# So it assumes a variety of things regarding file structure to make it sleeker.
+# Multihetsep requires called sites, so I also filter for heterozygous sites as that is all it needs.
+starting_dir = "/home/eriks/primatediversity/data/gVCFs_recalling_10_12_2024/"
+metadata_dir = "/home/eriks/primatediversity/data/gVCFs_recalling_10_12_2024_metadata/"
+size_cutoff = 10000000
+
+
+hetsep_dir = "steps/multihetsep"
+cobraa_dir = "steps/cobraa"
+
+# I do not distinguish between ind mask/call in this workflow.
+
+
+def generate_multihetsep(refname, vcf, contig, mask, callability):
+    out_path = contig+".txt"
+    inputs = [vcf, mask, callability]
+    outputs = ["steps/multihetsep/"+refname+"/"+out_path]
+    options = {
+        "cores": 2,
+        "memory": "14g",
+        "walltime": "12:00:00",
+        "account": "baboondiversity"
+    }
+    spec = """
+    bcftools view -Ou -s {refname} -r {contig} {vcf} | bcftools view -Oz -v snps -g ^miss > steps/multihetsep/{refname}/{contig}_temp.vcf.gz
+    grep "{contig}" {mask} > steps/multihetsep/{refname}/{contig}_temp.bed
+    python cobraa/msmc-tools/generate_multihetsep.py --mask=steps/multihetsep/{refname}/{contig}_temp.bed steps/multihetsep/{refname}/{contig}_temp.vcf.gz>steps/multihetsep/{refname}/{out_path}
+    rm steps/multihetsep/{refname}/{contig}_temp.vcf.gz
+    rm steps/multihetsep/{refname}/{contig}_temp.bed
+    """.format(vcf=vcf, refname=refname, contig=contig, mask=mask, out_path=out_path)
+    #print(spec)
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+
+def cobraa_run(multihetsep_l, ts, te):
+    refname = multihetsep_l[0].split("/")[-1].split("_")[0]
+    out_path = "steps/cobraa/"+refname+"_D50_ts{}_te{}_".format(ts, te)
+    inputs = multihetsep_l
+    outputs = [out_path+"final_parameters.txt"]
+    options = {
+        "cores": 3,
+        "memory": "30g",
+        "walltime": "12:00:00",
+        "account": "baboondiversity"
+    }
+    spec = """
+    python cobraa/cobraa.py -in {infiles} -o {out_path} -D 50 -b 25 -ts {ts} -te {te} -its 20 -spread_1 0.025 -spread_2 50 
+    """.format(infiles=" ".join(multihetsep_l), out_path=out_path, ts=ts, te=te)
+    # print(spec)
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+
+def cobraa_run_unstructured(multihetsep_l, chrom, mem=30):
+    refname = multihetsep_l[0].split("/")[-2]
+    out_path = "steps/cobraa/"+refname+"/"+chrom+"_"
+    inputs = multihetsep_l
+    outputs = [out_path+"final_parameters.txt"]
+    options = {
+        "cores": 3,
+        "memory": "{}g".format(mem),
+        "walltime": "12:00:00",
+        "account": "baboondiversity"
+    }
+    spec = """
+    python cobraa/cobraa.py -in {infiles} -o {out_path} -D 50 -b 25 -its 20 -spread_1 0.025 -spread_2 50 
+    """.format(infiles=" ".join(multihetsep_l), out_path=out_path)
+    # print(spec)
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+
+def get_ID_vcf(idx, target):
+    #print(target.spec)
+    filename = target.spec.split("/")[-2]+"_"+target.spec.split("/")[-1].split("_temp")[0]
+    return 'vcf_transform_{}'.format(filename)
+
+def get_ID_unstructured_cobraa(idx, target):
+    #print(target.spec)
+    filename = target.spec.split("/")[-2]+"_"+target.spec.split("/")[-1].split(" ")[0]
+    return 'unstructured_cobraa_{}'.format(filename)
+
+def get_ID_structured_cobraa(idx, target):
+    #print(target.spec)
+    filename = target.spec.split("/")[-1].split("_")[0]+"_"+target.spec.split("/")[-1].split("_")[2]+target.spec.split("/")[-1].split("_")[3]
+    #print(filename)
+    return 'structured_cobraa_{}'.format(filename)
+
+
+# Generating supporting directories
+os.makedirs(hetsep_dir, exist_ok=True)
+os.makedirs(cobraa_dir, exist_ok=True)
+
+metadata_dirs = glob.glob(metadata_dir+"*_individuals.txt")
+
+
+for d in metadata_dirs[:5]:
+    # Identify IDs
+    dir_metadata = pd.read_csv(d, sep="\t")
+    dir_metadata["gss"] = dir_metadata.GENUS+"_"+dir_metadata.SPECIES+"_"+dir_metadata.SUBSPECIES
+    # Slightly hacky way of preferring females and those with high coverage.
+    short_species = d.split("/")[-1].split("_")[0]
+    female_df = dir_metadata[pd.to_numeric(dir_metadata['AVG_COVERAGE_X'], errors='coerce').notnull()]
+    female_df = female_df.loc[(female_df.GENETIC_SEX == "F") & (female_df.AVG_COVERAGE_A >= 10)].sort_values(by="AVG_COVERAGE_A", ascending=False)
+    male_df = dir_metadata[pd.to_numeric(dir_metadata['AVG_COVERAGE_A'], errors='coerce').notnull()]
+    male_df = male_df.loc[~(male_df.GVCF_ID.isin(female_df.GVCF_ID)) & (male_df.AVG_COVERAGE_A >= 10)].sort_values(by="AVG_COVERAGE_A", ascending=False)
+    sorted_df = pd.concat([female_df, male_df])
+    if len(sorted_df) > 0:
+        sorted_input = sorted_df
+    else:
+        print("No usable ind for", short_species)
+        continue
+    region_metadata = pd.read_csv(metadata_dir+short_species+"_regions_and_batches.txt",
+                           sep="\t")
+    # Go through every unique genotype calling set.
+    for gvcf_folder in sorted_input.GVCF_FOLDER.unique():
+        # print(gvcf_folder)
+        # Pick the (currently one) best individuals in the metadata.
+        picked_inds = sorted_input.loc[sorted_input.GVCF_FOLDER == gvcf_folder].GVCF_ID.iloc[:1]
+        inds_cobraa = []
+        # Choose all paths based on the regions file.
+        batch_name = "{}/filteredVCF/bcf_step1/{}_batch{}_fploidy2_mploidy{}.bcf"
+        gvcfs_names = starting_dir+batch_name
+        mask_names = starting_dir+"{}/filteredVCF/pos_bed_cov_based/{}_batch{}_fploidy2_mploidy{}.bed"
+        multihetsep_args = []
+        # Workflow selecting all autosomal chromosomes above 1Mb in size.
+        aut_and_x = region_metadata.loc[(region_metadata.FEMALE_PLOIDY == 2) &
+                                     (region_metadata.END >= size_cutoff)]
+        # print(aut_and_x)
+        for c in aut_and_x.CONTIG_ID.unique()[:1]:
+            # Batches are either aut/Y-linked or X-linked.
+            mploidy = aut_and_x.loc[aut_and_x.CONTIG_ID == c].MALE_PLOIDY.iloc[0]
+            b = aut_and_x.loc[aut_and_x.CONTIG_ID == c].BATCH.iloc[0]
+            for i in picked_inds:
+                if os.path.exists(gvcfs_names.format(gvcf_folder, gvcf_folder, b, mploidy)):
+                    os.makedirs(hetsep_dir+"/"+i, exist_ok=True)
+                    ind_dir = {}
+                    ind_dir["refname"] = i
+                    ind_dir["vcf"] = gvcfs_names.format(gvcf_folder, gvcf_folder, b, mploidy)
+                    ind_dir["contig"] = c
+                    ind_dir["mask"] = mask_names.format(gvcf_folder, gvcf_folder, b, mploidy)
+                    ind_dir["callability"] = mask_names.format(gvcf_folder, gvcf_folder, b, mploidy)
+                    multihetsep_args.append(ind_dir)
+                    inds_cobraa.append(i)
+                else:
+                    print(gvcfs_names.format(gvcf_folder, gvcf_folder, b, mploidy), "does not exist")
+        # print(multihetsep_args)
+        multihetsep_o = gwf.map(generate_multihetsep, multihetsep_args, name=get_ID_vcf)
+        hetsep_l = []
+        for i in inds_cobraa:
+            os.makedirs(cobraa_dir+"/"+i, exist_ok=True)
+            # Aut run
+            c_list = []
+            for c in aut_and_x.CONTIG_ID.unique()[:1]:
+                c_list.append("steps/multihetsep/{}/{}.txt".format(i, c))
+                hetsep_l.append({"multihetsep_l": ["steps/multihetsep/{}/{}.txt".format(i, c)],
+                "chrom": c})
+            # hetsep_l.append({"multihetsep_l": c_list, "chrom": "_aut",
+            #                  "mem": 120})
+
+        #     x_list = []
+        #     for b in region_metadata.loc[(region_metadata.FEMALE_PLOIDY == 2) &
+        #                                  (region_metadata.MALE_PLOIDY == 1) &
+        #                                  (region_metadata.END >= 1000000)].BATCH.unique():
+        #         x_list.append("steps/multihetsep/{}/{}_batch{}.txt".format(i, i, b))
+        #     hetsep_l.append({"multihetsep_l": x_list, "bnames": "_chrX"})
+        # Unstructured
+        gwf.map(cobraa_run_unstructured, hetsep_l,
+                name=get_ID_unstructured_cobraa)
+#         # for i in range(10, 40, 2):
+#         #     for j in range(4, i-4, 2):
+#         #         gwf.map(cobraa_run, hetsep_l,
+#         #             name=get_ID_structured_cobraa,
+#         #             extra={"ts": j, "te": i})
+
+
+# Test of the old baboondiversity to compare.
+
+
+def generate_multihetsep_baboon(refname, vcf, mask, callability):
+    refname_batch = refname+"_"+vcf.split("/")[-1].split(".")[0]
+    multihetsep_name = refname_batch+".txt"
+    out_path = multihetsep_name
+    inputs = [vcf, mask, callability]
+    outputs = ["steps/multihetsep/"+refname+"/"+out_path]
+    options = {
+        "cores": 2,
+        "memory": "14g",
+        "walltime": "12:00:00",
+        "account": "baboondiversity"
+    }
+    spec = """
+    bcftools view -Ou -s {refname} {vcf} | bcftools view -Oz -g ^miss > steps/multihetsep/{refname}/{refname_batch}_temp.vcf.gz
+    python cobraa/msmc-tools/generate_multihetsep.py --negative_mask={mask} steps/multihetsep/{refname}/{refname_batch}_temp.vcf.gz>steps/multihetsep/{refname}/{out_path}
+    rm steps/multihetsep/{refname}/{refname_batch}_temp.vcf.gz
+    """.format(vcf=vcf, refname=refname, refname_batch=refname_batch, mask=mask, out_path=out_path)
+    #print(spec)
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+
+def cobraa_run_unstructured_baboon(multihetsep_l, bnames, mem=30):
+    refname = multihetsep_l[0].split("/")[-2]
+    out_path = "steps/cobraa/"+refname+bnames+"_"
+    inputs = multihetsep_l
+    outputs = [out_path+"final_parameters.txt"]
+    options = {
+        "cores": 3,
+        "memory": "{}g".format(mem),
+        "walltime": "12:00:00",
+        "account": "baboondiversity"
+    }
+    spec = """
+    python cobraa/cobraa.py -in {infiles} -o {out_path} -D 50 -b 100 -its 20
+    """.format(infiles=" ".join(multihetsep_l), out_path=out_path)
+    #print(spec)
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+
+def get_ID_unstructured_cobraa_baboon(idx, target):
+    print(target.spec)
+    filename = target.spec.split("/")[-1].split("_")[0]+"_"+target.spec.split("/")[-1].split(" ")[0]
+    return 'unstructured_cobraa_{}'.format(filename)
+
+
+# for d in ["/home/eriks/baboondiversity/people/eriks/second_analysis_baboons/data/Papio_metadata_with_clustering_sci.txt"]:
+#     # Identify IDs
+#     dir_metadata = pd.read_csv(d, sep =" ")
+#     picked_inds = ["PD_0214", "PD_0693"]
+#     for gvcf_folder in ["pass"]:
+#         vcf_name = "/home/eriks/baboondiversity/data/PG_panu3_phased_chromosomes_4_7_2021/chr{}/chr{}.phased.rehead.vcf.gz"
+#         mask_names = "/home/eriks/baboondiversity/data/callability_panu3_26_04_2021/chr{}sorted.bed.gz"
+#         multihetsep_args = []
+#         for b in ["{}".format(x) if x<= 20 else "X" for x in range(1, 22)]:
+#             for i in picked_inds:
+#                 os.makedirs(hetsep_dir+"/"+i, exist_ok=True)
+#                 ind_dir = {}
+#                 ind_dir["refname"] = i
+#                 ind_dir["vcf"] = vcf_name.format(b, b)
+#                 ind_dir["mask"] = mask_names.format(b)
+#                 ind_dir["callability"] = mask_names.format(b)
+#                 multihetsep_args.append(ind_dir)
+#         multihetsep_o = gwf.map(generate_multihetsep_baboon, multihetsep_args)
+#         hetsep_l = []
+#         for i in picked_inds:
+#             # Aut run
+#             b_list = []
+#             for b in range(1, 21):
+#                 b_list.append("steps/multihetsep/{}/{}_chr{}.txt".format(i, i, b))
+#                 hetsep_l.append({"multihetsep_l": ["steps/multihetsep/{}/{}_chr{}.txt".format(i, i, b)],
+#                 "bnames": "_chr{}".format(b)})
+#             hetsep_l.append({"multihetsep_l": b_list, "bnames": "_aut",
+#                              "mem": 60})
+
+#             x_list = []
+#             for b in ["X"]:
+#                 x_list.append("steps/multihetsep/{}/{}_chr{}.txt".format(i, i, b))
+#             hetsep_l.append({"multihetsep_l": ["steps/multihetsep/{}/{}_chr{}.txt".format(i, i, b)],
+#                 "bnames": "_chr{}".format(b)})
+#         # Unstructured
+#         gwf.map(cobraa_run_unstructured_baboon, hetsep_l,
+#                 name=get_ID_unstructured_cobraa_baboon)
---
+++ sgkit_workflow.py
@@ -0,0 +1,148 @@
+from gwf import Workflow, AnonymousTarget
+import os
+import pandas as pd
+import glob
+
+gwf = Workflow()
+
+starting_dir = "/home/eriks/primatediversity/data/gVCFs_recalling_10_12_2024/{}/filteredVCF/bcf_step1/"
+metadata_path = "/home/eriks/primatediversity/data/gVCFs_recalling_10_12_2024_metadata/"
+zarr_dir = "zarr_data/"
+
+def generate_zarr(focus_chr, all_chr, out_path):
+    inputs = [all_chr]
+    o = out_path+focus_chr+"/call_genotype"
+    outputs = [o]
+    options = {
+        "cores": 4,
+        "memory": "30g",
+        "walltime": "120:00:00",
+        "account": "baboondiversity"
+    }
+    spec = """
+    bcftools view -Ou -r {focus_chr} {all_chr} > {temp_bcf}
+    bcftools index {temp_bcf}
+    vcf2zarr convert {temp_bcf} {out_path}
+    rm {temp_bcf}
+    rm {temp_bcf}.csi
+    """.format(focus_chr=focus_chr, all_chr=all_chr,
+               temp_bcf=out_path+focus_chr+"_temp.bcf", out_path=out_path+focus_chr)
+    # print(spec)
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+
+def get_ID_vcf(idx, target):
+    #print(target.spec)
+    filename = target.spec.split("/")[-2].split(".")[0]+"_"+target.spec.split("/")[-1].split(".")[0]
+    return 'vcf_zarr_{}'.format(filename)
+
+
+# This section goes through all metadata folders and identifies the corresponding VCFs to use.
+
+metadata_folders = glob.glob(metadata_path+"*_individuals.txt")
+#print(metadata_folders)
+
+for folder in metadata_folders:
+    metadata_df = pd.read_csv(folder, sep="\t")
+    short_form = folder.split("/")[-1].split("_")[0]
+    regions_df = pd.read_csv(metadata_path+"{}_regions_and_batches.txt".format(short_form), sep="\t")
+    for GVCF_FOLDER in metadata_df.GVCF_FOLDER.unique():
+        #print(GVCF_FOLDER)
+        reference = metadata_df.loc[metadata_df.GVCF_FOLDER == GVCF_FOLDER].REFERENCE_FOLDER.unique()
+        all_chr_file = starting_dir.format(GVCF_FOLDER)+"{}_all_chr.sorted.bcf".format(GVCF_FOLDER)
+        if not os.path.exists(all_chr_file):
+            print("Problem with ", GVCF_FOLDER)
+            continue
+        zarr_input = []
+        # Chromosome X
+        zarr_input.extend(regions_df.loc[(regions_df.END >= 1000000) &
+                                      (regions_df.FEMALE_PLOIDY == 2) &
+                                      (regions_df.MALE_PLOIDY == 1) &
+              (regions_df.REFERENCE_FOLDER == reference[0])].CONTIG_ID.unique())
+        # Autosomes
+        zarr_input.extend(regions_df.loc[(regions_df.END >= 1000000) &
+                                      (regions_df.FEMALE_PLOIDY == 2) &
+                                      (regions_df.MALE_PLOIDY == 2) &
+              (regions_df.REFERENCE_FOLDER == reference[0])].CONTIG_ID.unique()[:1])
+        out_path = zarr_dir+GVCF_FOLDER+"/"
+        os.makedirs(out_path, exist_ok=True)
+        gwf.map(generate_zarr, zarr_input, name=get_ID_vcf,
+                extra={"all_chr": all_chr_file, "out_path": out_path})
+
+
+# test_dir = [{"refname": "Papio_cynocephalus_ssp", "out_path": "data/Papio_cynocephalus_ssp/"},
+#             {"refname": "Gorilla_gorilla_ssp", "out_path": "data/Gorilla_gorilla_ssp/"}]
+
+# gwf.map(generate_zarr, test_dir, name=get_ID_vcf)
+
+# # GVCF implementation. Will use it to bash together the different anubis/mulatta aligned species.
+
+# def generate_zarr_gvcf(focus_chr, chr_list, samples, out_path):
+#     inputs = [chr_list]
+#     o = out_path+focus_chr+"/call_genotype"
+#     outputs = [o]
+#     options = {
+#         "cores": 4,
+#         "memory": "30g",
+#         "walltime": "120:00:00",
+#         "account": "baboondiversity"
+#     }
+#     spec = """
+#     bcftools merge -Ou -r {focus_chr} {chr_list} |
+#     bcftools view -Ou -s {samples} |
+#     bcftools view -Ou -m2 -M2 |
+#     bcftools view -Ou -i 'MAF[0]>=0.1' |
+#     bcftools +prune -Ob -m LD=0.5 -w 1000bp --random-seed 10 -o {temp_bcf}
+#     bcftools index {temp_bcf}
+#     vcf2zarr convert --force {temp_bcf} {out_path}
+#     rm {temp_bcf}
+#     rm {temp_bcf}.csi
+#     """.format(focus_chr=focus_chr, chr_list=" ".join(chr_list), samples=",".join(samples),
+#                temp_bcf=out_path+focus_chr+"2_temp.bcf",
+#                out_path=out_path+focus_chr)
+#     # print(spec)
+#     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+
+# def get_ID_vcf_gvcf(idx, target):
+#     #print(target.spec)
+#     filename = target.spec.split("/")[-2].split(".")[0]+"_"+target.spec.split("/")[-1].split(".")[0]
+#     return 'vcf_zarr_{}'.format(filename)
+
+
+# metadata_folders_pca = ['/home/eriks/primatediversity/data/gVCFs_recalling_10_12_2024_metadata/Papio_individuals.txt',
+#                         '/home/eriks/primatediversity/data/gVCFs_recalling_10_12_2024_metadata/Macaca_individuals.txt'] # glob.glob(metadata_path+"Pa*_individuals.txt")
+# starting_dir_pca = "/home/eriks/primatediversity/data/gVCFs_recalling_10_12_2024/{}/gVCF/"
+
+
+# for folder in metadata_folders_pca:
+#     metadata_df = pd.read_csv(folder, sep="\t")
+#     short_form = folder.split("/")[-1].split("_")[0]
+#     regions_df = pd.read_csv(metadata_path+"{}_regions_and_batches.txt".format(short_form), sep="\t")
+#     metadata_df = metadata_df.loc[metadata_df.REFERENCE_FOLDER.isin(["Papio_anubis_ssp", "Macaca_mulatta_ssp"])]
+#     for REFERENCE_FOLDER in metadata_df.REFERENCE_FOLDER.unique():
+#         reference = REFERENCE_FOLDER
+#         gvcfs = metadata_df.GVCF_FOLDER.unique()
+#         print(reference)
+#         zarr_input = []
+#         regions_df = regions_df.loc[regions_df.REFERENCE_FOLDER == reference]
+#         aut_and_x = regions_df.loc[(regions_df.FEMALE_PLOIDY == 2) &
+#                                    (regions_df.END >= 1000000)]
+#         inds = metadata_df.loc[(metadata_df.AVG_COVERAGE_A >= 20)].GVCF_ID
+#         print(len(inds))
+#         # print(aut_and_x)
+#         for c in aut_and_x.CONTIG_ID.unique():
+#             # Batches are either aut/Y-linked or X-linked.
+#             mploidy = aut_and_x.loc[aut_and_x.CONTIG_ID == c].MALE_PLOIDY.iloc[0]
+#             b = aut_and_x.loc[aut_and_x.CONTIG_ID == c].BATCH.iloc[0]
+#             #print(c, mploidy, b)
+#             in_paths = [starting_dir_pca.format(x)+"{}_batch_{}_fploidy_2_mploidy_{}_gt.gvcf.gz".format(x, b, mploidy) for x in gvcfs]
+#             d = {}
+#             d["focus_chr"] = c
+#             d["chr_list"] = in_paths
+#             zarr_input.append(d)
+#         out_path = zarr_dir+reference+"_pca/"
+#         os.makedirs(out_path, exist_ok=True)
+#         gwf.map(generate_zarr_gvcf, zarr_input, name=get_ID_vcf_gvcf,
+#                 extra={"samples": inds, "out_path": out_path})
+
